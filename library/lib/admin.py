31cf9e963ba68f6c25e17b5a2e404d161b304346
Cwame-tech <cwameagyapong@gmail.com>
2021-02-22 02:08:23 +0000

    Adding Templates

--- library/lib/admin.py
+++ library/lib/admin.py
@@ -23,7 +23,7 @@ admin.site.register(Author, AuthorAdmin)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 # Register the Admin classes for BookInstance using the decorator
-@admin.register(BookInstance)
+
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
