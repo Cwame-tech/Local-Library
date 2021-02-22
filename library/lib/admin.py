b2586b587b91484ca2a82354613608c0290f7407
Cwame-tech <cwameagyapong@gmail.com>
2021-02-22 01:14:57 +0000

    Registering application to admin site

--- library/lib/admin.py
+++ library/lib/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models mport author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+
 
 # Register your models here.
