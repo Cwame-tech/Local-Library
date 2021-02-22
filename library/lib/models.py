d4c64b15715d8f15e35110890da563db9608e9fb
Cwame-tech <cwameagyapong@gmail.com>
2021-02-22 01:55:08 +0000

    Configuring list view and adding filter

--- library/lib/models.py
+++ library/lib/models.py
@@ -52,6 +52,7 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
+    
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -59,6 +60,10 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'  
     
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
